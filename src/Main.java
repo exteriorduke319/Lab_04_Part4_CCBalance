ac04e9633dcf0f30d8c5d18a45cad7bd753899f3
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-15 09:46:05 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final double interest = 1.17;
+        int ccInitBalance = 5000;
+        double monthOne;
+        double monthTwo;
+
+        System.out.println("The initial balance is $" + ccInitBalance);
+        monthOne = ccInitBalance * interest;
+        System.out.println("The balance after one month is $" + monthOne);
+        monthTwo = monthOne * interest;
+        System.out.println("The balance after two months is $" + monthTwo);
+
     }
 }
